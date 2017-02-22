fde9e7223f857b846712d6d116a451ed05f40985
Willian Pinho <willjrpp@gmail.com>
2017-02-22 11:18:11 -0300

    Add Facebook Controllers

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, render_template, send_from_directory
+from flask import url_for, request, session, redirect
+from flask_oauth import OAuth
 
 #----------------------------------------
 # initialization
@@ -33,6 +35,55 @@ def sucess():
 @app.route('/image_test')
 def test():
   return render_template('test.html')
+
+#----------------------------------------
+# facebook authentication
+#----------------------------------------
+
+FACEBOOK_APP_ID = '1276374105779390'
+FACEBOOK_APP_SECRET = 'f5b0355b0cbc57a454468fb48e06ab99'
+
+oauth = OAuth()
+
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': ('email, ')}
+)
+
+@facebook.tokengetter
+def get_facebook_token():
+    return session.get('facebook_token')
+
+def pop_login_session():
+    session.pop('logged_in', None)
+    session.pop('facebook_token', None)
+
+@app.route("/facebook_login")
+def facebook_login():
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next'), _external=True))
+
+@app.route("/facebook_authorized")
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    next_url = request.args.get('next') or url_for('index')
+    if resp is None or 'access_token' not in resp:
+        return redirect(next_url)
+
+    session['logged_in'] = True
+    session['facebook_token'] = (resp['access_token'], '')
+
+    return redirect(next_url)
+
+@app.route("/logout")
+def logout():
+    pop_login_session()
+    return redirect(url_for('index'))
 
 #----------------------------------------
 # launch
