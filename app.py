1ef5e303c26065ee9da3476205b2ea99c47ad836
Willian Pinho <willjrpp@gmail.com>
2017-02-22 11:41:00 -0300

    Add Picture

--- app.py
+++ app.py
@@ -52,9 +52,9 @@ def facebook_authorized(resp):
             request.args['error_description']
         )
     session['oauth_token'] = (resp['access_token'], '')
-    me = facebook.get('me?fields=picture.height(300)')
-    return 'Logged in as id=%s name=%s redirect=%s' % \
-        (me.data['id'], me.data['name'], request.args.get('next'))
+    me = facebook.get('me?fields=picture.height(300),name')
+    return 'Logged in as id=%s name=%s url=%s edirect=%s' % \
+        (me.data['id'], me.data['name'], me['picture']['data']['url'],request.args.get('next'))
 
 @facebook.tokengetter
 def get_facebook_oauth_token():
