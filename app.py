6b005aa71a6cdf486145229cf1cb3c5b36220e56
Willian Pinho <willjrpp@gmail.com>
2017-02-22 02:34:30 -0300

    Add templates

--- app.py
+++ app.py
@@ -6,5 +6,22 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate', method=['POST','GET'])
+def generate(dados):
+    return render_template('generate.html')
+ 
+@app.route('/sucess')
+def sucess():
+  return render_template('sucess.html')
+
+@app.route('/image_test')
+def test():
+  return render_template('test.html')
+
+
 if __name__ == '__main__':
     app.run()
