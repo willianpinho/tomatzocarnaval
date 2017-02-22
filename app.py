03de79d8feb357527cc5d6d0e4a886b5715cfd6a
Willian Pinho <willjrpp@gmail.com>
2017-02-22 13:17:51 -0300

    Add SQL

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, render_template, send_from_directory, url_for, session, request, redirect
 from flask_oauth import OAuth
+from flask.ext.sqlalchemy import SQLAlchemy
+
 
 SECRET_KEY = '5\x07\x0bO\x84\x0c\x99\x8b\xf7\xf8\xbd \xce\xd3\xd9\xfa\x16\x19)\x89\x01\xc7W6'
 DEBUG = True
@@ -13,6 +15,9 @@ FACEBOOK_APP_SECRET = 'f5b0355b0cbc57a454468fb48e06ab99'
 app = Flask(__name__)
 app.debug = DEBUG
 app.secret_key = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
 oauth = OAuth()
 
 #----------------------------------------
