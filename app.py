418e7c5d68bcf05fc51f15932a9e66d20d581caa
Willian Pinho <willjrpp@gmail.com>
2017-02-22 13:36:23 -0300

    Add Create DB

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ oauth = OAuth()
 #----------------------------------------
 # User Table
 #----------------------------------------
-
+from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
