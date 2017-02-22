fb7c1bc8e81183e232486f9ddc28973d3ebb8aa1
Willian Pinho <willjrpp@gmail.com>
2017-02-22 11:13:55 -0300

    Add Favicon and others controllers

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
+
+#----------------------------------------
+# initialization
+#----------------------------------------
 app = Flask(__name__)
+
+app.config.update(
+    DEBUG = True,
+)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 @app.route('/')
 def index():
@@ -16,6 +33,11 @@ def sucess():
 @app.route('/image_test')
 def test():
   return render_template('test.html')
+
+#----------------------------------------
+# launch
+#----------------------------------------
+
 
 if __name__ == '__main__':
     app.run()
