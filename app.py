1e03f2ba3f42916a191759f4394b8fd2bc6f208a
Willian Pinho <willjrpp@gmail.com>
2017-02-22 02:45:10 -0300

    Changes Index Route

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello():
-    return "Hello World!"
-
 @app.route('/')
 def index():
     return render_template('index.html')
