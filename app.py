56b3f46336b2da00ab2c34fb079dab97249e3ed2
Willian Pinho <willjrpp@gmail.com>
2017-02-23 00:46:11 -0300

    Testing PIL 12

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os, boto3, StringIO, re, urllib
+import os, boto3, StringIO, re, urllib, cStringIO
 from io import BytesIO
 from flask import Flask, render_template, send_from_directory, url_for, session, request, redirect, abort, send_file
 from flask_oauth import OAuth
